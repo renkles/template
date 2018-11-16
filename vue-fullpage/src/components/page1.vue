3a561dbd7771b57d301aeb9d301060f9320953e0
renkles <44504760+renkles@users.noreply.github.com>
2018-11-16 17:23:00 +0800

    add files

---
+++ vue-fullpage/src/components/page1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <p class="empty"></p>
+    <p class="content">page1</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page1'
+}
+</script>
+
+<style scoped>
+  .container {
+    background-color: #108F84;
+  }
+  .content {
+    display: inline-block;
+    width: 100%;
+    font-size: 3em;
+    letter-spacing: 2px;
+    text-align: center;
+    margin: 0;
+    vertical-align: middle;
+  }
+  .empty {
+    display: inline-block;
+    width: 0;
+    height: 100%;
+    vertical-align: middle;
+  }
+</style>
