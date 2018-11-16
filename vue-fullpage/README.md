00ea09ab84422d69c9152ce1918dd986733de104
renkles <44504760+renkles@users.noreply.github.com>
2018-11-16 21:04:14 +0800

    add files

---
+++ vue-fullpage/README.md
@@ -0,0 +1,20 @@
+# vue-fullpage
+  [vue-fullpage模版预览](https://renkles.github.io/template/dist/index.html)
+
+## images
+   <img src="./public/images/demo_1.jpg" width="762" height="328" /> <img src="./public/images/demo_2.jpg" width="762" height="500" />
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ vue-fullpage/src/components/changeBtn.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="change_btn" @click="changeType">
+    <div class="change_cube" :style="cubeStyle">{{text}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'change',
+  data () {
+    return {
+      text: undefined,
+      cubeStyle: {
+        left: undefined
+      }
+    }
+  },
+  props: {
+    type: String
+  },
+  created () {
+    this.init(this.type)
+  },
+  watch: {
+    type: function (val) {
+      this.init(val)
+    }
+  },
+  methods: {
+    init (msg) {
+      if (msg === 'horizontal') {
+        this.cubeStyle.left = '50%'
+        this.text = 'H'
+      } else {
+        this.cubeStyle.left = '0%'
+        this.text = 'V'
+      }
+    },
+    changeType () {
+      this.$emit('change')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @cube-color: #337AB7;
+  @btn-color: rgba(238,238,238,.5);
+  .change_btn {
+    position: fixed;
+    display: inline-block;
+    top: 3%;
+    right: 2%;
+    width: 40px;
+    height: 20px;
+    border-radius: 10px;
+    cursor: pointer;
+    background-color: @btn-color;
+    .change_cube {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      transition: all .5s ease;
+      background-color: @cube-color;
+      text-align: center;
+      line-height: 20px;
+      font-weight: bold;
+      color: #eee;
+    }
+  }
+</style>
